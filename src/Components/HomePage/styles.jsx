520fa6e7d54d9862860641aca2419f3e2a6b11a4
talhacarbonteq01 <muhammad.talha@carbonteq.com>
2023-07-20 13:15:20 +0500

    style change in hovering

--- src/Components/HomePage/styles.jsx
+++ src/Components/HomePage/styles.jsx
@@ -3,17 +3,17 @@ export const theme = Object.freeze({
     token: {
       // primary color
       colorPrimary: "#5C469C",
-      colorLink: "green",
+      colorLink: "blue",
       // hover color
       colorPrimaryHover: "#5C469C",
       colorLinkActive: "#00AA67",
       colorLinkHover: "#00AA67",
       wireframe: false,
       fontSize: 14,
-      borderRadius: 4,
+      borderRadius: 6,
       sizeStep: 4,
       fontFamily: `'Nunito', 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      'Ubuntu','Helvetica Neue',
       sans-serif`,
       lineHeight: 1.5714285714285714,
       colorError: "#ED0131"
